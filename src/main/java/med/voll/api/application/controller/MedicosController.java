de8e72a79c736eea58b07c0255650dc6ac36d2c6
André Luiz Pinheiro <andre.pinheiro@serpro.gov.br>
2025-01-22 14:49:43 +0000

    Procedendo com a exclusão física do registro

--- src/main/java/med/voll/api/application/controller/MedicosController.java
+++ src/main/java/med/voll/api/application/controller/MedicosController.java
@@ -16,7 +16,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -53,5 +53,11 @@ public class MedicosController {
     public void atualizar(@PathVariable String id, @RequestBody @Valid MedicoDTO dadosCadastroMedico) {
         var medico = medicoRepository.getReferenceById(Long.parseLong(id));
         medico.atualizar(MedicoFactory.convertFromDto(dadosCadastroMedico));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void deletar(@PathVariable String id) {
+        medicoRepository.deleteById(Long.parseLong(id));
     }
 }
